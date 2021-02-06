c9e432552c71948b0139c7c7e8f8cbb2fa94087b
otegami <a.s.takuya1026@gmail.com>
2021-02-06 13:35:01 +0900

    1.2: course information, step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -12,14 +12,27 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
+      <Part
+        name={ props.part1 }
+        number={ props.exercises1 }
+      />
+      <Part
+        name={ props.part2 }
+        number={ props.exercises2 }
+      />
+      <Part
+        name={ props.part3 }
+        number={ props.exercises3 }
+      />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return(
+    <div>
       <p>
-        { props.part1 } { props.exercises1 }
-      </p>
-      <p>
-        { props.part2 } { props.exercises2 }
-      </p>
-      <p>
-        { props.part3 } { props.exercises3 }
+        { props.name } { props.number }
       </p>
     </div>
   )
