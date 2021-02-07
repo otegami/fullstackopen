03ac14066eb7f361a94e975e775994981733d7ca
otegami <a.s.takuya1026@gmail.com>
2021-02-07 22:19:00 +0900

    A more complex state, debugging React apps

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,37 +1,56 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({ counter }) => <div>{ counter }</div>
-
-const Button = ({ handleClick, text }) => {
-  return (
-    <button onClick={ handleClick }>
-      { text }
-    </button>
-  )
-}
-
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
 
   return (
     <div>
-      <Display counter={ counter } />
-      <Button
-        handleClick={ increaseByOne }
-        text="plus"
+      button press history: { allClicks.join(' ') }
+    </div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={ onClick }>
+    { text }
+  </button>
+)
+
+const App = () => {
+  const [ left, setLeft ] = useState(0)
+  const [ right, setRight ] = useState(0)
+  const [ allClicks, setAll ] = useState([])
+
+  const handleLeftClick = () => { 
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => { 
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      { left }
+      <Button 
+        onClick={ handleLeftClick }
+        text="left"
       />
-      <Button
-        handleClick={ setToZero }
-        text="zero"
+      <Button 
+        onClick={ handleRightClick }
+        text="right"
       />
-      <Button
-        handleClick={ decreaseByOne }
-        text="minus"
-      />
+      { right }
+      <History allClicks={allClicks} />
     </div>
   )
 }
