fb825b8ac13e0bfa1950330788f69738ff92f4da
otegami <a.s.takuya1026@gmail.com>
2021-02-13 12:22:05 +0900

    Finish 2.5: separate module

--- part2/kurssitiedot/src/components/Content.js
+++ part2/kurssitiedot/src/components/Content.js
@@ -3,8 +3,7 @@ import Part from './Part';
 import Total from './Total';
 
 const Content = ({course}) => {
-  // TODO key の値をユニークなものに変更したい
-  const parts = course.parts.map(part => <Part key={part.name} part={ part } />)
+  const parts = course.parts.map(part => <Part key={part.id} part={ part } />)
 
   return (
     <div>
--- part2/kurssitiedot/src/components/Total.js
+++ part2/kurssitiedot/src/components/Total.js
@@ -4,7 +4,7 @@ const Total = ({ parts }) => {
   const sum = parts.reduce((sum, part) => sum + part.exercises, 0)
 
   return (
-    <p>Number of exercises { sum }</p>
+    <p>Total of exercises { sum }</p>
   )
 }
 
