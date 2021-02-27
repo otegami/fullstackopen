3e11eecea8a8e4d9a83d2a7ca4c4cba1675b3c62
otegami <t.kodama@everyleaf.com>
2021-02-27 22:03:29 +0900

    Finished Part3-a including all exercises

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -3,6 +3,20 @@ const app = express()
 
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(requestLogger)
+
 let notes = [
   {
     id: 1,
@@ -75,6 +89,8 @@ app.delete('/api/notes/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,14 @@
+const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :response-time ms :body'))
+
 
 let persons = [
   {
@@ -26,8 +33,75 @@ let persons = [
   }
 ]
 
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0
+  const randomId = Math.floor(Math.random() * (1000 - maxId) + maxId)
+
+  return randomId + 1
+}
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if(!person) {
+    return response.status(400).json({
+      error: 'There is no person you want to see!'
+    })
+  }
+
+  response.status(200).json(person)
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body) {
+    return response.status(400).json({
+      error: 'Your request is invalid! Sorry please try it again.'
+    })
+  }
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: `Your request is not enough imformation to add new person.`
+    })
+  }
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: `Name must be unique.`
+    })
+  }
+
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.get('/info', (request, response) => {
+  const totalNumber = persons.length
+  const date = new Date()
+
+  response.send(`<p>Phonebook has info for ${totalNumber} people<p><p>${date}<p>`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(reqeust.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
