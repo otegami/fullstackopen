3b62efc31ddf72379da654383ad9c5985a8e2a08
otegami <t.kodama@everyleaf.com>
2021-03-05 20:15:41 +0900

    Pure functions, immutable

--- part6/simple-note/src/index.js
+++ part6/simple-note/src/index.js
@@ -4,8 +4,7 @@ import { createStore } from 'redux'
 
 const noteReducer = (state = [], action) => {
   if (action.type === 'NEW_NOTE') {
-    state.push(action.data)
-    return state
+    return state.concat(action.data)
   }
 
   return state
