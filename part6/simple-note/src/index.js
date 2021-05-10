79486ffca77ab1594288b990eb2163a2d6ddecd0
otegami <t.kodama@everyleaf.com>
2021-05-11 08:31:41 +0900

    restart to learn step 6
    I have just decided to learn react every morning.

--- part6/simple-note/src/index.js
+++ part6/simple-note/src/index.js
@@ -1,23 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux'
+import { store } from './store'
 import App from './App'
-import noteService from './services/notes'
-
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-const store = createStore(reducer, composeWithDevTools())
-
-noteService.getAll().then(notes => 
-  store.dispatch({ type: 'NEW_NOTE', data: notes })
-)
 
 ReactDOM.render(
   <Provider store={store}>
