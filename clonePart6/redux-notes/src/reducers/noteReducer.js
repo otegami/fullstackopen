fb70fdd795dda6896288a55ff32ca4ad100a7399
otegami <t.kodama@everyleaf.com>
2021-05-12 08:32:15 +0900

    WIP noteReducer をテストファーストで作成していく

---
+++ clonePart6/redux-notes/src/reducers/noteReducer.js
@@ -0,0 +1,10 @@
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    state.push(action.data)
+    return state
+  }
+
+  return state
+}
+
+export default noteReducer
