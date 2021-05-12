ede36f6d6a62a17d23162083fb4e6fa0f2dacc33
otegami <t.kodama@everyleaf.com>
2021-05-13 08:36:29 +0900

    noteReducer をテストファーストで実装

--- clonePart6/redux-notes/src/reducers/noteReducer.js
+++ clonePart6/redux-notes/src/reducers/noteReducer.js
@@ -1,10 +1,21 @@
 const noteReducer = (state = [], action) => {
-  if (action.type === 'NEW_NOTE') {
-    state.push(action.data)
-    return state
+  switch(action.type) {
+    case 'NEW_NOTE':
+      return state.concat(action.data)
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.data.id
+      const noteToChange = state.find(n => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+      return state.map(note =>
+        note.id !== id ? note : changedNote
+      )
+    }
+    default:
+      return state
   }
-
-  return state
 }
 
 export default noteReducer
