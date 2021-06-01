0ac2a11aa8e45551a8d52ebcea8df22201711ade
otegami <a.s.takuya1026@gmail.com>
2021-06-01 05:35:18 -0700

    Separeted the logic to set initial data from App component in Anecdotes

--- clonePart6/redux-anecdotes/src/App.js
+++ clonePart6/redux-anecdotes/src/App.js
@@ -4,16 +4,13 @@ import Filter from './components/Filter'
 import Notification from './components/Notification'
 import NewAnecdote from './components/NewAnecdote'
 import AnecdoteList from './components/AnecdoteList'
-import anecdoteService from './services/anecdotes'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+    dispatch(initializeAnecdotes())
   }, [dispatch])
 
   return (
