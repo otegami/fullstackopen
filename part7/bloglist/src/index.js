9073ceb36b0f11148bd2c77ace37c6df7dd8a848
otegami <a.s.takuya1026@gmail.com>
2021-06-02 16:23:09 -0700

    Implemented Note App with React Router's function

--- part7/bloglist/src/index.js
+++ part7/bloglist/src/index.js
@@ -2,25 +2,103 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import {
   BrowserRouter as Router,
-  Switch, Route, Link
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useHistory,
+  useRouteMatch
 } from "react-router-dom"
+
+const Login = (props) => {
+  const history = useHistory()
+  const onSubmit = (event) => {
+    event.preventDefault()
+    props.onLogin('otegami')
+    history.push('/')
+  }
+
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input />
+        </div>
+        <div>
+          password: <input type='password'/>
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </div>
+  )
+}
 
 const Home = () => (
   <div><h2>TKTL notes app</h2></div>
 )
 
-const Notes = () => (
-  <div><h2>Notes</h2></div>
+const Notes = ({ notes }) => (
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      {notes.map(note =>
+        <li key={note.id}>
+          <Link to= { `/notes/${note.id}` }>
+            {note.content}
+          </Link>
+        </li>
+      )}
+    </ul>
+  </div>
 )
+
+const Note = ({ note }) => {
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div>
+        <strong>
+          {note.important ? 'important' :  ''}
+        </strong>
+      </div>
+    </div>
+  )
+}
 
 const Users = () => (
   <div><h2>Users</h2></div>
 )
 
 const App = () => {
-  const padding = {
-    padding: 5
-  }
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only Javascript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
+    }
+  ])
+  const [user, setUser] = useState(null)
+  const login = user => { setUser(user) }
+  const padding = { padding: 5 }
+  const match = useRouteMatch('/notes/:id')
+  const note = match
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null
 
   return (
     <Router>
@@ -28,14 +106,24 @@ const App = () => {
         <Link style={padding} to="/">home</Link>
         <Link style={padding} to="/notes">notes</Link>
         <Link style={padding} to="/users">users</Link>
+        {user
+          ? <em>{user} logged in</em>
+          : <Link style={padding} to="/login">login</Link>
+        }
       </div>
 
       <Switch>
+        <Route path="/notes/:id">
+          <Note note={note} />
+        </Route>
         <Route path="/notes">
-          <Notes />
+          <Notes notes={notes} />
         </Route>
         <Route path="/users">
-          <Users />
+          {user ? <Users /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login">
+          <Login onLogin={login}/>
         </Route>
         <Route path="/">
           <Home />
@@ -43,10 +131,16 @@ const App = () => {
       </Switch>
 
       <div>
-        <i>Note app, Department of Computer Science 2021</i>
+        <br />
+        <em>Note app, Department of Computer Science 2021</em>
       </div>
     </Router>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
