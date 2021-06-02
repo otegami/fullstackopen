163fd3a47e106c1da578d0f3a60361f62617c304
otegami <a.s.takuya1026@gmail.com>
2021-06-02 04:58:26 -0700

    Implemented framework of blog

--- part7/bloglist/src/App.js
+++ part7/bloglist/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-function App() {
+const Home = () => {
+  <div><h2>TKTL notes app</h2></div>
+}
+
+const Notes = () => {
+  <div><h2>Notes</h2></div>
+}
+
+const Users = () => {
+  <div><h2>Users</h2></div>
+}
+
+const App = () => {
+  const [page, setPage] = useState('home')
+
+  const toPage = (page) => (event) => {
+    event.preventDefault()
+    setPage(page)
+  }
+
+  const content = () => {
+    if (page === 'home') {
+      return <Home />
+    } else if (page === 'notes') {
+      return <Notes />
+    } else if (page === 'users') {
+      return <Users />
+    }
+  }
+
+  const padding = {
+    padding: 5
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <div>
+        <a href="" onClick={toPage('home')} style={padding}>
+          home
         </a>
-      </header>
+        <a href="" onClick={toPage('notes')} style={padding}>
+          notes
+        </a>
+        <a href="" onClick={toPage('users')} style={padding}>
+          users
+        </a>
+      </div>
+
+      { content() }
     </div>
-  );
+  )
 }
 
 export default App;
