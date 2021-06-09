980753079af8de94a309c60838402a33b9d8198e
otegami <a.s.takuya1026@gmail.com>
2021-06-09 00:29:37 -0700

    Reorganized App component from each separated components

--- part7/bloglist/src/App.js
+++ part7/bloglist/src/App.js
@@ -1,55 +1,80 @@
 import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
-
-const Home = () => {
-  <div><h2>TKTL notes app</h2></div>
-}
-
-const Notes = () => {
-  <div><h2>Notes</h2></div>
-}
-
-const Users = () => {
-  <div><h2>Users</h2></div>
-}
+import {
+  Switch,
+  Route,
+  Link,
+  Redirect,
+  useRouteMatch
+} from "react-router-dom"
+import Home from './components/Home'
+import Users from './components/Users'
+import Login from './components/Login'
+import Notes from './components/Notes'
+import Note from './components/Note'
 
 const App = () => {
-  const [page, setPage] = useState('home')
-
-  const toPage = (page) => (event) => {
-    event.preventDefault()
-    setPage(page)
-  }
-
-  const content = () => {
-    if (page === 'home') {
-      return <Home />
-    } else if (page === 'notes') {
-      return <Notes />
-    } else if (page === 'users') {
-      return <Users />
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only Javascript',
+      important: false,
+      user: 'Matti Luukkainen'
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas'
     }
-  }
-
-  const padding = {
-    padding: 5
-  }
+  ])
+  const [user, setUser] = useState(null)
+  const login = user => { setUser(user) }
+  const padding = { padding: 5 }
+  const match = useRouteMatch('/notes/:id')
+  const note = match
+    ? notes.find(note => note.id === Number(match.params.id))
+    : null
 
   return (
     <div>
       <div>
-        <a href="" onClick={toPage('home')} style={padding}>
-          home
-        </a>
-        <a href="" onClick={toPage('notes')} style={padding}>
-          notes
-        </a>
-        <a href="" onClick={toPage('users')} style={padding}>
-          users
-        </a>
+        <Link style={padding} to="/">home</Link>
+        <Link style={padding} to="/notes">notes</Link>
+        <Link style={padding} to="/users">users</Link>
+        {user
+          ? <em>{user} logged in</em>
+          : <Link style={padding} to="/login">login</Link>
+        }
       </div>
 
-      { content() }
+      <Switch>
+        <Route path="/notes/:id">
+          <Note note={note} />
+        </Route>
+        <Route path="/notes">
+          <Notes notes={notes} />
+        </Route>
+        <Route path="/users">
+          {user ? <Users /> : <Redirect to="/login" />}
+        </Route>
+        <Route path="/login">
+          <Login onLogin={login}/>
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+      <div>
+        <br />
+        <em>Note app, Department of Computer Science 2021</em>
+      </div>
     </div>
   )
 }
