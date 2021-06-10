1a2ec8ba4bf97861800b59a976cd02d3f6f6386e
otegami <a.s.takuya1026@gmail.com>
2021-06-10 04:21:44 -0700

    Implemented simple counter function

--- part7/learn-webpack/src/App.js
+++ part7/learn-webpack/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 
-const App = () => (
-  <div className="container">
-    Hello Webpack
-  </div>
-)
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  return (
+    <div className="container">
+      Hello webpack { counter } clicks
+      <button onClick={() => setCounter(counter + 1)}>
+        press
+      </button>
+    </div>
+  )
+}
 
 export default App
