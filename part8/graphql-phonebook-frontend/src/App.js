962827245af37a1c3a8222d79a578cdc69ca7305
otegami <a.s.takuya1026@gmail.com>
2021-06-16 16:47:36 -0700

    Handling mutation errors by graphql

--- part8/graphql-phonebook-frontend/src/App.js
+++ part8/graphql-phonebook-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_PERSONS } from './queries'
 
@@ -6,16 +6,38 @@ import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
+  const [errorMessage, setErrormessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrormessage(message)
+    setTimeout(() => {
+      setErrormessage(null)
+    }, 10000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage}/>
       <Persons persons = { result.data.allPersons }/>
-      <PersonForm />
+      <PersonForm setError={notify}/>
+    </div>
+  )
+}
+
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
+  }
+
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
     </div>
   )
 }
