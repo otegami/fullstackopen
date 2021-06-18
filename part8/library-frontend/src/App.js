6b9773509342b394193e76fba999e0a20ba7a3b2
otegami <a.s.takuya1026@gmail.com>
2021-06-18 04:47:07 -0700

    Implemented the function to get all authors

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,11 +1,19 @@
-
 import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
+
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { ALL_AUTHORS } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -17,6 +25,7 @@ const App = () => {
 
       <Authors
         show={page === 'authors'}
+        authors={result.data.allAuthors}
       />
 
       <Books
--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,11 +1,10 @@
-  
 import React from 'react'
 
-const Authors = (props) => {
-  if (!props.show) {
+const Authors = ({show, authors}) => {
+
+  if (!show) {
     return null
   }
-  const authors = []
 
   return (
     <div>
