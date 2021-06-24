e0e79f4aa49f63525dec8c4c77ef0a24de0cdc0e
otegami <a.s.takuya1026@gmail.com>
2021-06-24 16:52:54 -0700

    Implemented login function

--- part8/mongo/person-backend.js
+++ part8/mongo/person-backend.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
-const { ApolloServer, UserInputError, gql } = require('apollo-server')
+
+const { ApolloServer, UserInputError, gql, AuthenticationError } = require('apollo-server')
+const { v1: uuid } = require('uuid')
+const jwt = require('jsonwebtoken')
+
 const mongoose = require('mongoose')
 const Person = require('./models/person')
+const User = require('./models/user')
 
 const MONGODB_URI = process.env.MONGODB_URI
+const JWT_SECRET = process.env.JWT_SECRET
 
 console.log('connecting to', MONGODB_URI)
 
@@ -33,10 +39,21 @@ const typeDefs = gql`
     NO
   }
 
+  type User {
+    username: String!
+    friends: [Person!]!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
   type Query {
     personCount: Int!
     allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
+    me: User
   }
 
   type Mutation {
@@ -50,6 +67,16 @@ const typeDefs = gql`
       name: String!
       phone: String!
     ): Person
+    createUser(
+      username: String!
+    ): User
+    login(
+      username: String!
+      password: String!
+    ): Token
+    addAsFriend(
+      name: String!
+    ): User
   }
 `
 
@@ -63,8 +90,8 @@ const resolvers = {
 
       return Person.find({ phone: { $exists: args.phone === 'YES' } })
     },
-    findPerson: (root, args) =>
-      Person.findOne({ name: args.name })
+    findPerson: (root, args) => { return Person.findOne({ name: args.name }) },
+    me: (root, args, context) => { return context.currentUser }
   },
   Person: {
     address: (root) => {
@@ -75,11 +102,17 @@ const resolvers = {
     }
   },
   Mutation: {
-    addPerson: async (root, args) => {
+    addPerson: async (root, args, context) => {
       const person = new Person({ ...args })
+      const currentUser = context.currentUser
+
+      if (!currentUser) {
+        throw new AuthenticationError("note authenticated")
+      }
 
       try {
         await person.save()
+        currentUser.friends = currentUser.friends.concat(person)
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args
@@ -99,6 +132,47 @@ const resolvers = {
         })
       }
       return person
+    },
+    createUser: (root, args) => {
+      const user = new User({ username: args.username })
+
+      return user.save()
+        .catch(error => {
+          throw new UserInputEroror(error.message, {
+            invalidArgs: args
+          })
+        })
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+
+      if ( !user || args.password !== 'secret' ) {
+        throw new UserInputError("wrong credentials")
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
+    },
+    addAsFriend: async (root, args, { currentUser }) => {
+      const  nonFriendAlready = (person) =>
+        !currentUser.frineds.map(f => f._id).includes(person._id)
+
+      if (!currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
+
+      const person = await Person.findOne({ name: args.name })
+      if ( nonFriendAlready(person) ) {
+        currentUser.friends = currentUser.friends.concat(person)
+      }
+
+      await currentUser.save()
+
+      return currentUser
     }
   }
 }
@@ -106,6 +180,16 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bear ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id).populate('friends')
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
