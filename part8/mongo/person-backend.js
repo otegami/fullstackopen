a9df8da5fb01a40e9ef260dbd85e86b7b501d72b
otegami <a.s.takuya1026@gmail.com>
2021-06-28 04:16:12 -0700

    Implemented friend list function

--- part8/mongo/person-backend.js
+++ part8/mongo/person-backend.js
@@ -113,6 +113,7 @@ const resolvers = {
       try {
         await person.save()
         currentUser.friends = currentUser.friends.concat(person)
+        await currentUser.save()
       } catch (error) {
         throw new UserInputError(error.message, {
           invalidArgs: args
